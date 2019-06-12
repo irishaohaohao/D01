f20a693952bd4407b4c96bb4b0432289efa4a41f
irishaohaohao <33838885+irishaohaohao@users.noreply.github.com>
2019-06-12 02:49:46 -0400

    Add files via upload

---
+++ app.js
@@ -0,0 +1,68 @@
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
+
+var app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+
+//add here to create the logic of new page, new page in views
+app.use(function(req,res,next){
+  console.log("about to look for routes!!!")
+  //console.dir(req.hearers)
+  next()
+});
+
+app.get('/', function(req, res, next) {
+  res.render('index',{title:"Dogworld Demo"});
+});
+
+app.get('/beachcomment', function(req, res, next) {
+  res.render('beachcomment',{title:"Comment a Beach"});
+});
+
+function processFormData(req,res,next){
+  console.dir(req.body)
+  res.render('process',
+    {title:"Form Data", BeachName:req.body.BeachName, coms:req.body.comment, score:req.body.inlineRadioOptions});
+}
+app.post('/process', processFormData);
+
+app.use(function(req,res,next){
+  console.log("about to look for post routes!!!")
+  next()
+});
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;
